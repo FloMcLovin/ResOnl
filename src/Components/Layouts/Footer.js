850acc205d6828b181bea761a0293355b12134e4
FloMcLovin <60557496+FloMcLovin@users.noreply.github.com>
2020-02-10 14:03:38 +0100

    Footer
    Adjustments Footer

--- src/Components/Layouts/Footer.js
+++ src/Components/Layouts/Footer.js
@@ -1,32 +1,108 @@
 import React from "react";
-import { Grid, Paper, Container, Link } from "@material-ui/core";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
+
+function Kontakt() {
+  return (
+    <Typography variant="subtitle1" color="initial" display="inline">
+      <Link color="inherit" href="https://reserviere.online/kontakt">
+        Kontakt
+      </Link>
+      {"  --  "}
+    </Typography>
+  );
+}
+
+function Rechtliches() {
+  return (
+    <Typography variant="subtitle1" color="initial" display="inline">
+      <Link color="inherit" href="https://reserviere.online/rechtliches">
+        Rechtliches
+      </Link>
+      {"  --  "}
+    </Typography>
+  );
+}
+
+function CookieRichtlinien() {
+  return (
+    <Typography variant="subtitle1" color="initial" display="inline">
+      <Link color="inherit" href="https://reserviere.online/cookies">
+        Cookie Richtlinien
+      </Link>
+      {"  --  "}
+    </Typography>
+  );
+}
+
+function Datenschutzrichtlinie() {
+  return (
+    <Typography variant="subtitle1" color="initial" display="inline">
+      <Link color="inherit" href="https://reserviere.online/datenschutz">
+        Datenschutz
+      </Link>
+      {"  --  "}
+    </Typography>
+  );
+}
+
+function Impressum() {
+  return (
+    <Typography variant="subtitle1" color="initial" display="inline">
+      <Link color="inherit" href="https://reserviere.online/impressum">
+        Impressum
+      </Link>{" "}
+    </Typography>
+  );
+}
+
+function Copyright() {
+  return (
+    <Typography variant="subtitle2" color="textSecondary" display="initial">
+      {"Copyright © Reserviere.Online "}
+      {new Date().getFullYear()}
+    </Typography>
+  );
+}
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: "bottom",
-    height: "100%",
-    backgroundColor: "yellow"
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100vh"
   },
-  container: {
-    position: "absolute",
-    bottom: "0",
-    width: "100%"
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2)
+  },
+  footer: {
+    padding: "auto",
+    color: "#rgba(255, 215, 0, 1)",
+    backgroundColor: "rgb(48, 63, 159)",
+    marginTop: "auto",
+    textAlign: "center"
   }
 }));
 
-export default function Footer() {
+export default function StickyFooter() {
   const classes = useStyles();
 
   return (
-    <Container className={classes.container}>
-      <Grid container>
-        <Grid item xs={12}>
-          <Paper elevation={3}>
-            <Link> Impressum </Link>
-          </Paper>
-        </Grid>
-      </Grid>
-    </Container>
+    <div className={classes.root}>
+      <CssBaseline />
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Kontakt />
+          <Rechtliches />
+          <CookieRichtlinien />
+          <Datenschutzrichtlinie />
+          <Impressum />
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
   );
 }
